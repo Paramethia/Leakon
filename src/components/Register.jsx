ae1682c3156a8672c3597c9c8c5fb435ca4e7bfb
Kyrin <143742919+Paramethia@users.noreply.github.com>
2024-08-14 01:03:27 +0200

    Redirect API and remove email verification.

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -71,7 +71,7 @@ const Register = () => {
                             </label>
                             <input
                                 type="text"
-                                placeholder="Enter Name"
+                                placeholder="Create username"
                                 className="form-control block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm"
                                 id="exampleInputName"
                                 onChange={(event) => setName(event.target.value)}
