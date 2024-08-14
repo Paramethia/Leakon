c0df8f4144a95154e9dd1dfeb16a4ef968ccbd40
Kyrin <143742919+Paramethia@users.noreply.github.com>
2024-08-15 00:44:10 +0200

    Fix link generator

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -212,7 +212,7 @@ const Component = () => {
 
             <main className="flex-1 p-8 space-y-6" style={isDarkMode ? darkModeStyles : lightModeStyles}>
                 <div
-                    className="flex fixed relative px-3 mb-5 items-center justify-between md:justify-start"
+                    className="flex static relative px-3 mb-5 items-center justify-between md:justify-start"
                     style={{
                         backgroundColor: isDarkMode ? '#101424' : '#282434',
                         left: 0,
