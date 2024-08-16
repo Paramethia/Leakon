49e02cfc3d19b30d6fa5dbc821be58de31dc76b2
Kyrin <143742919+Paramethia@users.noreply.github.com>
2024-08-17 00:25:28 +0200

    Revert

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -28,7 +28,6 @@ const Register = () => {
 
     const handleRegister = (event) => {
         event.preventDefault();
-        localStorage.removeItem("inviteLink");
     
         axios.post('https://invicon-back-end.onrender.com/register', { username, email, password, inviteId })
             .then(result => {
