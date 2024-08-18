f69ac45e6ed96a7138d45a92361bd9395874abe2
Kyrin <143742919+Paramethia@users.noreply.github.com>
2024-08-18 17:27:39 +0200

    Add user context api

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './components/App'
-// import Footer from './components/Footer'
+import UserProvider from './components/UserContext';
+import Footer from './components/Footer';
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App/>
-    {/* <Footer/> */}
-  </React.StrictMode>
+  <UserProvider>
+    <React.StrictMode>
+      <App />
+      {/* <Footer /> */}
+    </React.StrictMode>
+  </UserProvider>
 )
