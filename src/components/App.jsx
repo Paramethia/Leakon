e5162e1e1f2ed773dfe13afc298abb7fdeb4de07
Kyrin <143742919+Paramethia@users.noreply.github.com>
2024-08-18 17:19:28 +0200

    Remove Context api

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,7 +7,6 @@ import RequestPasswordReset from './RequestPasswordReset';
 import ResetPassword from './ResetPassword';
 import Dashboard from './Dashboard';
 import Rewards from './Rewards';
-import UserProvider from './UserContext';
 
 function App() {
   return (
