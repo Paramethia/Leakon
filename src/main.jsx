944b2d80262098ab8df708344882b136daab5eb5
Kyrin <143742919+Paramethia@users.noreply.github.com>
2024-08-18 21:44:10 +0200

    Add footer

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,12 @@
 import React, { useState } from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './components/App'
-// import { UserContext } from './components/UserContext';
 import Footer from './components/Footer';
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    {/* <UserContext.Provider value={{ user, setUser }}> */}
       <App />
-      {/* <Footer /> */}
-    {/* </UserProvider> */}
+      <Footer />
   </React.StrictMode>
 )
