c8596f0a9543fda05c42e231ea0605d86e06e603
Kyrin <143742919+Paramethia@users.noreply.github.com>
2024-08-18 20:29:49 +0200

    Import footer

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,15 @@
-import React, { useState, useContext } from 'react'
+import React, { useState } from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './components/App'
-import { UserContext, UserProvider } from './components/UserContext';
+//import { UserProvider } from './components/UserContext';
 import Footer from './components/Footer';
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <UserProvider>
+    {/* <UserProvider> */}
       <App />
       {/* <Footer /> */}
-    </UserProvider>
+    {/* </UserProvider> */}
   </React.StrictMode>
 )
