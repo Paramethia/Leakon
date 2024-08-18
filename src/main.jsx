17d5fdd48c3d8eea65d42df53d70c6317306511c
Kyrin <143742919+Paramethia@users.noreply.github.com>
2024-08-18 17:43:05 +0200

    Fucken hell

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './components/App'
-import { UserProvider } from './components/UserContext'; // Use named import
+import { UserContext, UserProvider } from './components/UserContext';
 import Footer from './components/Footer';
 
 
