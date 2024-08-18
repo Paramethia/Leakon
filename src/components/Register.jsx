94c7707162619d516f7802629ecb5c8fa5fc3301
Kyrin <143742919+Paramethia@users.noreply.github.com>
2024-08-19 01:33:18 +0200

    Replace image url

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -128,7 +128,7 @@ const Register = () => {
         <ToastContainer />
 
         <div className="flex h-screen overflow-hidden">
-            <div className="hidden md:block md:w-1/2 bg-auto" style={{ backgroundImage: 'url(https://res.cloudinary.com/dw7w2at8k/image/upload/v1720626946/Home_1_d6rirw.png)' }}></div>
+            <div className="hidden md:block md:w-1/2 bg-auto" style={{ backgroundImage: 'url(https://res.cloudinary.com/doxalk3ms/image/upload/v1723931827/Invicon_register_or_log_in_image_xfeg1y.png)' }}></div>
             <div className="w-full md:w-1/2 flex flex-col items-center justify-center bg-gray-100">
                 <h1 className="block md:hidden mb-6 text-4xl font-bold text-dark">Invicon</h1>
                 <div className="bg-white p-8 rounded shadow-md w-3/4 animate__animated animate__fadeInRight">
