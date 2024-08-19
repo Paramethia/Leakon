fa0be923e6777e5f5e89e72ef97a89244c72c2cb
Kyrin <143742919+Paramethia@users.noreply.github.com>
2024-08-19 13:26:36 +0200

    Add max length to input

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -140,6 +140,7 @@ const Register = () => {
                             </label>
                             <input
                                 type="text"
+                                maxlength="12"
                                 placeholder="Create username"
                                 className="form-control block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm"
                                 id="exampleInputName"
@@ -153,6 +154,7 @@ const Register = () => {
                             </label>
                             <input
                                 type="email"
+                                maxlength="14"
                                 placeholder="Enter Email"
                                 className="form-control block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm"
                                 id="exampleInputEmail1"
