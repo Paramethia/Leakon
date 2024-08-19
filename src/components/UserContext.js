2a9b23eb00169ca6f63ec4923856626ae2d357df
Kyrin <143742919+Paramethia@users.noreply.github.com>
2024-08-19 12:58:02 +0200

    Delete UserProvider function

--- src/components/UserContext.js
+++ src/components/UserContext.js
@@ -3,14 +3,3 @@ import React, { createContext, useState } from 'react';
 // Create the context
 export const UserContext = createContext();
 
-// Create a provider component
-export const UserProvider = ({ children }) => {
-    const [user, setUser] = useState({ username: '', email: '' });
-/*
-    return (
-        <UserContext.Provider value={{ user, setUser }}>
-            {children}
-        </UserContext.Provider>
-    );
-*/
-};
