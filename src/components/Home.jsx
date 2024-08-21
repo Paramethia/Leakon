351c554cb6f50dd42ecde40d37edc8dcd89ec6f9
Kyrin <143742919+Paramethia@users.noreply.github.com>
2024-08-21 10:54:09 +0200

    Fix side bar bug

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -21,7 +21,7 @@ const Header = () => {
 const Sidebar = ({ isOpen, toggleSidebar }) => {
     let inviteLink = localStorage.getItem('inviteLink');
     let code = "ABC123";
-    if (inviteLink) code = inviteLink.slice(-8);
+    if (inviteLink) code = inviteLink.slice(-8)
 
     const handleCopyReferralCode = () => {
         navigator.clipboard.writeText(inviteLink);
