63c5b40ff752fea89e0aad49325c67c111cda7ba
Kyrin <143742919+Paramethia@users.noreply.github.com>
2024-08-24 13:00:55 +0200

    Blur

--- src/components/Rewards.jsx
+++ src/components/Rewards.jsx
@@ -234,7 +234,7 @@ const Rewards = () => {
                   key={index}
                   className="group relative flex flex-col items-start justify-between rounded-lg bg-white p-4 shadow-md transition-all hover:bg-gray-100 dark:bg-gray-950 dark:hover:bg-gray-800"
                 >
-                  <div className={"flex-1 w-full relative" onClick={() => toggleSpoiler(index)}>
+                  <div className="flex-1 w-full relative" onClick={() => toggleSpoiler(index)}>
                     <video ref={(el) => (videoRefs.current[index] = el)}  src={link} className={`w-full h-full object-cover rounded-lg ${!spoilers[index] ? "blur-lg" : ""}`} controls={false} />
                     <button onClick={() => playorpause(index)} className="absolute inset-0 flex items-center justify-center bg-black bg-opacity-50 text-white text-3xl">
                       {playingIndex === index ? <FaPause /> : <FaPlay />}
