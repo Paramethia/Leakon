97059fd97f67f1659a7eed905822b04024766b77
Kyrin <143742919+Paramethia@users.noreply.github.com>
2024-08-26 14:47:47 +0200

    Tier rewards description

--- src/components/Rewards.jsx
+++ src/components/Rewards.jsx
@@ -302,7 +302,7 @@ const Rewards = () => {
                     href={rewardLinks[tier]}
                     target="_blank"
                     rel="noopener noreferrer"
-                    className="text-center bg-[#282434] text-white font-bold py-2 px-4 rounded transition-colors hover:bg-[#3c3a4e]"
+                    className="block text-center bg-[#282434] text-white font-bold py-2 px-4 rounded transition-colors hover:bg-[#3c3a4e]"
                   >
                     <button onMouseOver={moan.play()} className="bg-gray-500 hover:bg-blue-500 dark:bg-gray-700 text-gray-900 dark:text-white px-4 py-2 rounded-md w-full">
                       🔓 Get tier {tier} reward
@@ -311,7 +311,7 @@ const Rewards = () => {
                 )))}
   
                 {currentTier < 5 && (
-                  <a href="https://t.me/daemozon" className="text-center bg-[#282434] text-white font-bold py-2 px-4 rounded transition-colors hover:bg-[#3c3a4e] w-full">
+                  <a href="block https://t.me/daemozon" className="text-center bg-[#282434] text-white font-bold py-2 px-4 rounded transition-colors hover:bg-[#3c3a4e] w-full">
                     <button className="bg-gray-500 hover:bg-blue-500 dark:bg-gray-700 text-gray-900 dark:text-white px-4 py-2 rounded-md">
                       Buy a tier 🧧
                     </button>
