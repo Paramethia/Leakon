60c6ba7b9ed56d8226e675808d4ea2536fc15c4f
Kyrin <143742919+Paramethia@users.noreply.github.com>
2024-08-26 14:40:42 +0200

    Tier rewards description

--- src/components/Rewards.jsx
+++ src/components/Rewards.jsx
@@ -302,17 +302,17 @@ const Rewards = () => {
                     href={rewardLinks[tier]}
                     target="_blank"
                     rel="noopener noreferrer"
-                    className="inline-block text-center bg-[#282434] text-white font-bold py-2 px-4 rounded transition-colors hover:bg-[#3c3a4e] w-full"
+                    className="text-center bg-[#282434] text-white font-bold py-2 px-4 rounded transition-colors hover:bg-[#3c3a4e]"
                   >
-                    <button onMouseOver={moan.play()} className="Reward bg-gray-300 hover:bg-blue-500 dark:bg-gray-700 text-gray-900 dark:text-white px-4 py-2 rounded-md">
+                    <button onMouseOver={moan.play()} className="bg-gray-500 hover:bg-blue-500 dark:bg-gray-700 text-gray-900 dark:text-white px-4 py-2 rounded-md w-full">
                       🔓 Get tier {tier} reward
                     </button>
                   </a>
                 )))}
   
                 {currentTier < 5 && (
-                  <a href="https://t.me/daemozon">
-                    <button className="block bg-gray-300 hover:bg-blue-500 dark:bg-gray-700 text-gray-900 dark:text-white px-4 py-2 rounded-md">
+                  <a href="https://t.me/daemozon" className="text-center bg-[#282434] text-white font-bold py-2 px-4 rounded transition-colors hover:bg-[#3c3a4e] w-full">
+                    <button className="bg-gray-500 hover:bg-blue-500 dark:bg-gray-700 text-gray-900 dark:text-white px-4 py-2 rounded-md">
                       Buy a tier 🧧
                     </button>
                   </a>
