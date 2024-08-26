2a0380099e7b860b84b3e81fdffa1c732b6a3671
Kyrin <143742919+Paramethia@users.noreply.github.com>
2024-08-26 23:34:22 +0200

    Fix styling

--- src/components/Rewards.jsx
+++ src/components/Rewards.jsx
@@ -306,17 +306,17 @@ const Rewards = () => {
                     href={rewardLinks[tier]}
                     target="_blank"
                     rel="noopener noreferrer"
-                    className="block text-center bg-[#282434] text-white font-bold py-2 px-4 rounded transition-colors hover:bg-[#3c3a4e]"
+                    className="text-center bg-[#282434] text-white font-bold py-2 px-4 rounded transition-colors hover:bg-[#3c3a4e]"
                   >
-                    <button onMouseOver={moan.play()} className="bg-gray-500 hover:bg-blue-500 dark:bg-gray-700 text-gray-900 dark:text-white px-4 py-2 rounded-md w-full">
+                    <button onMouseOver={moan.play()} className="block bg-gray-500 hover:bg-blue-500 dark:bg-gray-700 text-gray-900 dark:text-white px-4 py-2 rounded-md w-full">
                       🔓 Get tier {tier} reward
                     </button>
                   </a>
                 )))}
   
                 {currentTier < 5 && (
-                  <a href="block https://t.me/daemozon" className="text-center bg-[#282434] text-white font-bold py-2 px-4 rounded transition-colors hover:bg-[#3c3a4e] w-full">
-                    <button className="bg-gray-500 hover:bg-blue-500 dark:bg-gray-700 text-gray-900 dark:text-white px-4 py-2 rounded-md">
+                  <a href="https://t.me/daemozon" className="text-center bg-[#282434] text-white font-bold py-2 px-4 rounded transition-colors hover:bg-[#3c3a4e] w-full">
+                    <button className="block bg-gray-500 hover:bg-blue-500 dark:bg-gray-700 text-gray-900 dark:text-white px-4 py-2 rounded-md">
                       Buy a tier 🧧
                     </button>
                   </a>
