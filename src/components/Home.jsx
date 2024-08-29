97256384d4475b44f5729e8c9c5cb94165853fb3
Kyrin <143742919+Paramethia@users.noreply.github.com>
2024-08-29 14:21:01 +0200

    Add class names

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -150,10 +150,9 @@ const InviteLinkComponent = () => {
                 {error && <p className="text-red-500">{error}</p>}
                 {!error && (
                     <>
-                    <div className="bg-gray-200 dark:bg-gray-800 rounded-md px-4 py-2 text-lg font-medium text-gray-700 dark:text-white">
+                    <div className="Link bg-gray-200 dark:bg-gray-800 rounded-md px-4 py-2 text-lg font-medium text-gray-700 dark:text-white">
                         {inviteLink}
                     </div>
-                    <br />
                     <button className="bg-gray-300 hover:bg-blue-500 dark:bg-gray-700 text-gray-900 dark:text-white px-4 py-2 rounded-md" onClick={handleCopy}>
                         Copy
                     </button>
