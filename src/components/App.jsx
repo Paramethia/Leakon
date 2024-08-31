e5cf356df72282e444f8eeb4a4f38d35c939e02d
Kyrin <143742919+Paramethia@users.noreply.github.com>
2024-09-01 00:53:43 +0200

    Update user context

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './Home';
 import Login from './Login';
