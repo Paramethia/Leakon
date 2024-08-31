197e31f5261238ba36d33e630d99d437e2345f95
Kyrin <143742919+Paramethia@users.noreply.github.com>
2024-08-31 23:31:42 +0200

    Update log in logic

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -71,7 +71,7 @@ const Login = () => {
                             </label>
                             <input
                                 type="text"
-                                maxlength="12"
+                                maxlength="14"
                                 placeholder="Enter username"
                                 className="form-control block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm"
                                 id="exampleInputEmail1"
@@ -85,7 +85,7 @@ const Login = () => {
                             </label>
                             <input
                                 type="password" 
-                                maxlength="14"
+                                maxlength="17"
                                 placeholder="Enter password"
                                 className="form-control block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm"
                                 id="exampleInputPassword1"
