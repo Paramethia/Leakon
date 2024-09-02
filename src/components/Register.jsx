ec13ccba95c8c58920172fd9ff61bc24af257368
Kyrin <143742919+Paramethia@users.noreply.github.com>
2024-09-02 18:34:21 +0200

    Update registration logic

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -64,9 +64,9 @@ const Register = () => {
                     navigate('/login');
                 }, 4400);
             } else if (result.data === "Username already taken.") {
-                SetUsernameError("This username is already in use.")
+                setUsernameError("This username is already in use.")
             } else if (result.data === "Registered.") {
-               if (usedInvite != null && alreadyReg) {
+               if (usedInvite && alreadyReg) {
                     toast.warn("You cannot use invite links if you already registered on this device.", {
                         position: "top-center",
                         autoClose: 5200,
