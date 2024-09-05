43e12fc5867d30725eb712c64f83d24e74f9f81d
Kyrin <143742919+Paramethia@users.noreply.github.com>
2024-09-05 16:00:27 +0200

    Add Vercel analytics

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,11 @@
 import React, { useState } from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './components/App'
+import { Analytics } from "@vercel/analytics/react"
+import { SpeedInsights } from "@vercel/speed-insights/react"
 
 ReactDOM.createRoot(document.getElementById('root')).render(
+  inject();
   <React.StrictMode>
       <App />
   </React.StrictMode>
