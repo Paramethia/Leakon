ba2a6c5730a04605b543c777cbdeec24917928c0
Kyrin <143742919+Paramethia@users.noreply.github.com>
2024-09-08 11:25:18 +0200

    Lol

--- src/components/Rewards.jsx
+++ src/components/Rewards.jsx
@@ -137,6 +137,11 @@ const Rewards = () => {
   const [isPaymentConOpen, setIsPaymentConOpen] = useState(false);
 
   let moan = new Audio('https://res.cloudinary.com/doxalk3ms/video/upload/v1724628435/ahh_sound_effect_gukkzc.mp4');
+  let rButton = document.getElementById('reward-button');
+  
+  rButton.addEventListener('mouseover', event => {
+      moan.play()
+  }}
 
   let NotLogged = () => {
         toast.error("You are not logged in.", {
@@ -369,7 +374,7 @@ const playorpause = (index) => {
                         rel="noopener noreferrer"
                         className="text-center bg-[#282434] text-white font-bold py-2 px-4 rounded transition-colors hover:bg-[#3c3a4e]"
                       >
-                        <button className="bg-gray-500 hover:bg-blue-500 dark:bg-gray-700 text-gray-900 dark:text-white px-4 py-2 rounded-md">
+                        <button id="reward-button" className="bg-gray-500 hover:bg-blue-500 dark:bg-gray-700 text-gray-900 dark:text-white px-4 py-2 rounded-md">
                           🔓 Get tier {tier} reward
                         </button>
                       </a>
