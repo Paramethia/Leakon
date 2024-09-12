07d6fe31046bb1d9af655244c0ca3a0c464be4ea
Kyrin <143742919+Paramethia@users.noreply.github.com>
2024-09-12 17:37:12 +0200

    Add files via upload

---
+++ src/components/Register.jsx
@@ -0,0 +1,199 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'animate.css';
+import { useEffect, useState, useContext } from 'react';
+import { Helmet } from "react-helmet";
+import { Link, useLocation } from "react-router-dom";
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
+import { UserContext } from './UserContext';
+import { toast, ToastContainer, Bounce, Flip, Slide } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import './Register.css';
+
+const Header = () => {
+    return ( 
+        <Helmet>
+            <title> Invicon - register </title>
+        </Helmet>
+    );
+};
+
+const Register = () => {
+    const location = useLocation();
+    const searchParams = new URLSearchParams(location.search);
+    let usedInvite = searchParams.get('inviteId');
+    const {username, setName} = useContext(UserContext);
+    const {email, setEmail} =  useContext(UserContext);
+    const [password, setPassword] = useState('');
+    const [usernameError, setUsernameError] = useState('');
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        let note = document.getElementById("Email-note");
+    
+        setTimeout(() => {
+            note.style.display = 'none'
+        }, 3800 );
+    });
+    
+    // To check if the user already has an account on the device to prevent creating and inviting multiple acccount on the same device.
+
+    let alreadyReg = localStorage.getItem("username");
+    let [warning, setWarning] = useState("");
+
+    const handleRegister = (event) => {
+        event.preventDefault();
+
+        // Regular expression to check for invalid characters in the username
+        let usernameVal = /^[a-zA-Z0-9._]+$/;
+
+        // Check for spaces or invalid characters in the username
+        if (!usernameVal.test(username)) {
+            setUsernameError("Username can only contain letters, numbers, underscores, or periods.");
+            return;
+        } else {
+            setUsernameError(''); // Clear the error if the username is valid
+        }
+
+        if (alreadyReg) {
+            setWarning("Don't create another account while already registered on this device! Fucking log in or reset your password if you forgot!!");
+            setTimeout(() => {
+                navigate('/login');
+            }, 5800);
+        } else if (usedInvite != null && alreadyReg) {
+            setWarning("You cannot use invite links if you already registered on this device.");
+            setTimeout(() => {
+                navigate('/login');
+            }, 4850);
+        } else {
+            // Construct the request body based on whether email is provided
+            const requestBody = {
+                username,
+                password,
+                usedInvite
+            };
+            if (email) {
+                requestBody.email = email;
+            }
+    
+            axios.post('https://invicon-back-end.onrender.com/register', requestBody)
+            .then(result => {
+                if (result.data === "Account already registered.") {
+                    toast.warn("Already registered, pal. Go log in", {
+                        position: "top-center",
+                        autoClose: 4000,
+                        hideProgressBar: false,
+                        closeOnClick: true,
+                        pauseOnHover: true,
+                        draggable: true,
+                        progress: undefined,
+                        theme: "dark",
+                        transition: Flip,
+                    });
+                    setTimeout(() => {
+                        navigate('/login');
+                    }, 4400);
+                } else if (result.data === "Username already taken.") {
+                    setUsernameError("Username already in use.")
+                } else if (result.data === "Registered.") {
+                    localStorage.setItem('usedInvite', usedInvite);
+                    localStorage.setItem("username", username);
+                    if (email) localStorage.setItem("email", email);
+                    navigate('/home');
+                }
+            })
+            .catch(err => {
+                console.log(err);
+                toast.error("Registration failed. Please try again later.", {
+                    position: "top-center",
+                    autoClose: 4000,
+                    hideProgressBar: false,
+                    closeOnClick: true,
+                    pauseOnHover: true,
+                    draggable: true,
+                    progress: undefined,
+                    theme: "dark",
+                    transition: Bounce,
+                });
+            });
+        }   
+    }
+  
+    function clearCache() {
+        localStorage.clear()
+    }
+   
+    return (
+        <>
+        
+        <Header />
+
+        <ToastContainer />
+
+        <div className="flex h-screen overflow-hidden">
+            <div className="hidden md:block md:w-1/2 bg-auto" style={{ backgroundImage: 'url(https://res.cloudinary.com/doxalk3ms/image/upload/v1723931827/Invicon_register_or_log_in_image_xfeg1y.png)' }}></div>
+            <div className="w-full md:w-1/2 flex flex-col items-center justify-center bg-gray-400">
+                <h1 className="block md:hidden mb-6 text-4xl font-bold text-dark">Invicon</h1>
+                <div className="bg-gray-300 p-8 rounded shadow-md w-3/4 animate__animated animate__fadeInRight">
+                    <h3 className="mb-6 text-2xl font-bold text-dark">Register</h3>
+                    <form onSubmit={handleRegister}>
+                        <div className="mb-4 text-left">
+                            <label htmlFor="exampleInputName" className="block text-sm font-bold mb-2">
+                                Username:
+                            </label>
+                            <input
+                                type="text"
+                                minlength="3"
+                                maxlength="14"
+                                placeholder="Create username"
+                                className="form-control block w-full bg-gray-200 px-3 py-2 border border-gray-500 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm"
+                                id="exampleInputName"
+                                onChange={(event) => setName(event.target.value)}
+                                required
+                            />
+                            {usernameError && <p className="text-red-500 text-sm mt-1">{usernameError}</p>}
+                        </div>
+                        <div className="mb-4 text-left">
+                            <label htmlFor="exampleInputEmai1" className="block text-sm font-bold mb-2">
+                                Email (optional):
+                            </label>
+                            <input
+                                type="email"
+                                minlengh="12"
+                                maxlength="35"
+                                placeholder="Enter email"
+                                className="form-control block w-full bg-gray-200 px-3 py-2 border border-gray-500 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm"
+                                id="exampleInputEmail1"
+                                onChange={(event) => setEmail(event.target.value)}
+                            />
+                        </div>
+                          <p Id="Email-note"> Ensure you remember your password if you don't put in your email. </p>
+                        <div className="mb-6 text-left">
+                            <label htmlFor="exampleInputPassword1" className="block text-sm font-bold mb-2">
+                                Password:
+                            </label>
+                            <input
+                                type="password"
+                                minlength="4"
+                                maxlength="17"
+                                placeholder="Create password"
+                                className="form-control block w-full bg-gray-200 px-3 py-2 border border-gray-500 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm"
+                                id="exampleInputPassword1"
+                                onChange={(event) => setPassword(event.target.value)}
+                                required
+                            />
+                        </div>
+                        {warning && <p className="text-red-500 text-sm mt-1">{warning}</p>}
+                        <button type="submit" className="w-full bg-dark text-white py-2 rounded-md transition duration-300 ease-in-out transform hover:scale-105"> Submit </button>
+                    </form>
+                    <p className="my-4 mx-2">Already have an account? <Link to='/login' className='text-dark'>Log in</Link></p>
+                    {/* <button onClick={clearCache} className="w-full bg-dark text-white py-2 rounded-md transition duration-300 ease-in-out transform hover:scale-105"> Clear cache </button> */}
+                </div>
+            </div>
+        </div>
+
+        </>
+    );
+}
+
+export default Register;
