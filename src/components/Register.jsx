7ea12a79d367f9c356725ac8a77d3e0e5969b6b3
Kyrin <143742919+Paramethia@users.noreply.github.com>
2024-09-14 02:20:22 +0200

    New background image

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -131,7 +131,7 @@ const Register = () => {
         <ToastContainer />
 
         <div className="flex h-screen overflow-hidden">
-            <div className="hidden md:block md:w-1/2 bg-auto" style={{ backgroundImage: 'url(https://res.cloudinary.com/doxalk3ms/image/upload/v1723931827/Invicon_register_or_log_in_image_xfeg1y.png)' }}></div>
+            <div className="hidden md:block md:w-1/2 bg-auto" style={{ backgroundImage: 'url(https://res.cloudinary.com/doxalk3ms/image/upload/v1726273058/Infamous_sophie_rain_spider-man_vid_image_v3ujms.png)' }}></div>
             <div className="w-full md:w-1/2 flex flex-col items-center justify-center bg-gray-400">
                 <h1 className="block md:hidden mb-6 text-4xl font-bold text-dark">Invicon</h1>
                 <div className="bg-gray-300 p-8 rounded shadow-md w-3/4 animate__animated animate__fadeInRight">
