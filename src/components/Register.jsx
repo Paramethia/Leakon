d7880bbfc90c9eb50ebd345fbec4ee802bc3d963
Kyrin <143742919+Paramethia@users.noreply.github.com>
2024-09-14 16:10:28 +0200

    Flex

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -131,10 +131,10 @@ const Register = () => {
         <ToastContainer />
 
         <div className="flex h-screen overflow-hidden">
-            <div className="hidden md:block md:w-1/2 bg-auto bg-black" style={{/* backgroundImage: 'url(https://res.cloudinary.com/doxalk3ms/image/upload/v1726273826/Infamous_sophie_rain_spider-man_vid_image_pdfyeb.png)', 
+            <div className="hidden flex items-center md:block md:w-1/2 bg-auto bg-black" style={{/* backgroundImage: 'url(https://res.cloudinary.com/doxalk3ms/image/upload/v1726273826/Infamous_sophie_rain_spider-man_vid_image_pdfyeb.png)', 
                  backgroundSize: 'cover', backgroundPosition: 'center', backfaceVisibility: 'hidden', backgroundRepeat: 'no-repeat', backgroundAttachment: 'fixed'                                                           
             */}}>
-               <center><video src="https://res.cloudinary.com/doxalk3ms/video/upload/v1726318414/Preview_slide_show_shortened_nm2d1x.mov" autoplay muted loop alt="Registration video"> You browser does not support the video tag. </video></center>
+               <video src="https://res.cloudinary.com/doxalk3ms/video/upload/v1726318414/Preview_slide_show_shortened_nm2d1x.mov" autoplay muted loop alt="Registration video"> You browser does not support the video tag. </video>
             </div>
             <div className="w-full md:w-1/2 flex flex-col items-center justify-center bg-gray-400">
                 <h1 className="block md:hidden mb-6 text-4xl font-bold text-dark">Invicon</h1>
