6c4bee08da419d5065752dcd42941f51d0019b7b
Kyrin <143742919+Paramethia@users.noreply.github.com>
2024-10-22 22:51:53 +0200

    Added log out function

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -13,7 +13,7 @@ import './Extra styles.css';
 const Header = () => {
     return ( 
         <Helmet>
-            <title> Invicon - home </title>
+            <title> Leakon - home </title>
             <meta name="description" content="Welcome to Invicon" />
         </Helmet>
     );
@@ -387,7 +387,7 @@ const Home = () => {
                     <Link to="/home" style={{ textDecoration: 'none' }} className="Logo flex-1">
                         <div className="text-white flex items-center gap-2 justify-center md:justify-start">
                             <img src="Invicon top bar logo.png" alt="Invicon Logo" className="w-8 h-8" />
-                            <h1 className="text-2xl font-bold font-helvetica">Invicon</h1>
+                            <h1 className="text-2xl font-bold font-helvetica">Leakon</h1>
                         </div>
                     </Link>
                     <div className="Theme">
